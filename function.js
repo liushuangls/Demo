4a1a9b72e3b24c51e3f9de9e9cd4f5bc326f5855
liushuang <liushuangbill@outlook.com>
2017-10-20 10:20:08 +0800

    function.js

---
+++ function.js
@@ -0,0 +1,31 @@
+// 获取DOM
+function $(select) {
+  if (select) {
+    try {
+      let node = document.querySelectorAll(select)
+      if (node.length === 1) {
+        return node[0]
+      } else {
+        return node
+      }
+    } catch (error) {
+      return null
+    }
+  } else {
+    return null
+  }
+}
+
+//获取元素的纵坐标（相对于窗口）
+function getTop(e){
+  var offset=e.offsetTop;
+  if(e.offsetParent!=null) offset+=getTop(e.offsetParent);
+  return offset;
+}
+
+//获取元素的横坐标（相对于窗口）
+function getLeft(e){
+  var offset=e.offsetLeft;
+  if(e.offsetParent!=null) offset+=getLeft(e.offsetParent);
+  return offset;
+}
